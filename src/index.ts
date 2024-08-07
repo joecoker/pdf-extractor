95e654e3687bb81da4fb3c6798a7a793fc2af58a
AkashDeep000 <akashdeepdas970@gmail.com>
2024-08-07 21:20:30 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv"
+import fs from "fs/promises"
+import getBillText from "./utils/getBillText.js";
+const app = express()
+
+dotenv.config()
+
+const pdfDownloadDir = process.env.DOWNLOAD_DIR || "./pdfs"
+const PORT = process.env.PORT || 3000
+
+const fileExists = async (filePath: string) => {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+// creating PDF download directory if exist
+if (!(await fileExists(pdfDownloadDir))) {
+    await fs.mkdir(pdfDownloadDir);
+}// const pdfBuffer = await getPdfBuffer(pdfUrl, pdfDownloadDir)
+
+
+app.get("/", async (req, res) => {
+    const pdfUrl = req.query.pdfUrl as string
+    if (!pdfUrl) {
+        res.status(400).json({
+            status: "failed",
+            message: "Please provide 'pdfUrl' as url parameter."
+        })
+    }
+
+    try {
+        const pdfText = await getBillText(pdfUrl, pdfDownloadDir)
+        res.json({
+            status: "success",
+            text: pdfText
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            status: "failed",
+            message: "Unexpected error",
+            error
+        })
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log("Server listining to PORT ", PORT);
+}).on("error", console.log)
